c0ac04e9a43073ce240b847d30f4d81f6ea5e471
赵凌云 <90768491+xianyunluolei@users.noreply.github.com>
2021-11-19 07:33:54 +0800

    Add files via upload

---
+++ learn.11.18.c
@@ -0,0 +1,49 @@
+//反序 (递归的方法)
+//#include<stdio.h>
+//void reverse(int x)
+//{
+//		if (x / 10 != 0)
+//		{
+//			printf("%d", x % 10);
+//			digit(x / 10);
+//		}
+//		else
+//		{
+//			printf("%d", x);
+//			return;
+//		}
+//}
+//int main()
+//{
+//	int num,i;
+//	int a = 0;
+//	scanf_s("%d", &num);
+//	reverse(num);
+//	return 0;
+//}
+
+#include<stdio.h>
+void division(int x)
+{
+	int num = 0;
+	if (x / 10 != 0)
+	{
+		num++;
+		division(x / 10);
+		printf("%d ",x % 10);
+	}
+	else
+	{
+		printf("%d ", x);
+		return;
+	}
+		
+}
+int main()
+{
+	int num;
+	printf("请输入位数小于等于五的数：>");
+	scanf_s("%d", &num);
+	division(num);
+	return 0;
+}
