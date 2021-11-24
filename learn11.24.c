3be7f0b7c23788c8f6ae1f9c2d7a5dfe02f770db
赵凌云 <90768491+xianyunluolei@users.noreply.github.com>
2021-11-25 07:54:06 +0800

    Add files via upload

---
+++ learn11.24.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<windows.h>
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//		Sleep(300);
+//	}
+//	
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
