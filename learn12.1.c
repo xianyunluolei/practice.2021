ae68194bb60ec8479e1b10e145f85c4d9143a7d9
赵凌云 <90768491+xianyunluolei@users.noreply.github.com>
2021-12-02 07:59:38 +0800

    Add files via upload

---
+++ learn12.1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+//void test1(int arr[3][5])
+//{
+//
+//}
+//void test2(int arr[3])
+//{
+//
+//}
+//void test3(int(*arr)[5])
+//{
+//
+//}
+//int main()
+//{
+//	int arr[3][5];
+//	test1(arr);
+//	return 0;
+//}
+void print(char * str)
+{
+	printf("%s", str);
+}
+int main()
+{
+	int a = 5;
+	char b = "w";
+	print("w");
+	return 0;
+}
