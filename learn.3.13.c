2d9f106457ccb563841c3f4cb111bacfd1e3b647
赵凌云 <90768491+xianyunluolei@users.noreply.github.com>
2022-03-14 13:13:24 +0800

    Add files via upload

---
+++ learn.3.13.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#define NUM 2
+#define n 2//小写字母也可以用与宏定义常量 
+
+//int main()
+//{
+//	int x;
+//	scanf_s("%d", &x);
+//	switch (x+NUM)
+//	{
+//	case 1:printf("1\n"); break;
+//	case 55:printf("2\n"); break;
+//	case 3:printf("3\n"); break;
+//	case NUM:printf("2\n"); break;
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	int a, b, c, i;
+	scanf_s("%d", &a);
+	return 0;
+}
